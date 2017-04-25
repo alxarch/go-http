19b8d910db78c988dfeedede00fb33ea3fb9f63e
Sigalas Alexandros <alxarch@gmail.com>
2017-04-25 15:32:07 +0300

    Allow single-string multiple hosts in vhosts

--- vhosts/vhosts.go
+++ vhosts/vhosts.go
@@ -2,19 +2,23 @@ package vhosts
 
 import (
 	"net/http"
+	"strings"
 	"sync"
 )
 
 type VirtualHosts struct {
-	vhosts         map[string]http.Handler
-	mu             sync.RWMutex
-	defaultHandler http.Handler
+	vhosts map[string]http.Handler
+	mu     sync.RWMutex
 }
 
 func NewVirtualHosts(vhosts map[string]http.Handler) *VirtualHosts {
 	v := &VirtualHosts{}
-	for host, h := range vhosts {
-		v.HandleHost(h, host)
+	for hosts, h := range vhosts {
+		for _, host := range strings.Split(hosts, " ") {
+			if host != "" {
+				v.HandleHost(h, host)
+			}
+		}
 	}
 	return v
 }
@@ -23,7 +27,7 @@ func (v *VirtualHosts) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	v.mu.RLock()
 	defer v.mu.RUnlock()
 	if v.vhosts != nil {
-		if handler := v.vhosts[r.Host]; handler != nil {
+		if handler, ok := v.vhosts[r.Host]; ok && handler != nil {
 			handler.ServeHTTP(w, r)
 			return
 		}
@@ -38,6 +42,10 @@ func (v *VirtualHosts) HandleHost(handler http.Handler, hosts ...string) {
 		v.vhosts = make(map[string]http.Handler)
 	}
 	for _, host := range hosts {
-		v.vhosts[host] = handler
+		for _, h := range strings.Split(host, " ") {
+			if h = strings.TrimSpace(h); h != "" {
+				v.vhosts[h] = handler
+			}
+		}
 	}
 }
