b8e36a0a1bd8d3f95970f947521388e6ebffa9ad
Sigalas Alexandros <alxarch@gmail.com>
2017-07-14 17:26:27 +0300

    Add assert.Method and assert.ContentType

--- assert/assert.go
+++ assert/assert.go
@@ -1,6 +1,9 @@
 package assert
 
-import "net/http"
+import (
+	"mime"
+	"net/http"
+)
 
 type Error struct {
 	StatusCode int
@@ -38,4 +41,24 @@ func Must(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func Method(m string, allow ...string) string {
+	for _, a := range allow {
+		if m == a {
+			return m
+		}
+	}
+	panic(Error{http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed)})
+}
+
+func ContentType(v string, allow ...string) (string, map[string]string) {
+	mt, params, err := mime.ParseMediaType(v)
+	NoError(err, http.StatusBadRequest)
+	for _, a := range allow {
+		if mt == a {
+			return mt, params
+		}
+	}
+	panic(Error{http.StatusUnsupportedMediaType, http.StatusText(http.StatusUnsupportedMediaType)})
 }
